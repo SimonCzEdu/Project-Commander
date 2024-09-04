dad7298dab677839109610c91ee7b7cffdbd9e44
Szymon Czapiewski <simonczedu@gmail.com>
2024-09-04 13:40:05 +0000

    Setup: checking for POST and GET methods

--- project_commander/views.py
+++ project_commander/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("App test!")
+def index(request):    
+    if request.method == "POST":
+        return HttpResponse("We are POSTING Baby!")
+    elif request.method == "GET":
+        return HttpResponse(request.method)
