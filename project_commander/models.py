81e9e6eb28c427357d52208db6f8a283acab1693
Szymon Czapiewski <simonczedu@gmail.com>
2024-09-23 13:01:59 +0000

    Create: first model draft for Categories

--- project_commander/models.py
+++ project_commander/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+TYPE = ((0, "Item"), (1, "List"))
+
 
 # Create your models here.
+class Categorie(models.Model):
+    category_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    category_type = models.IntegerField(choices=TYPE, default=0)
